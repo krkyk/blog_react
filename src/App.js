fa3218199b916946d72bf0400ad9562a26d5bf3b
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-28 21:13:50 +0900

    ログイン機能作成

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import CreatePost from "./components/CreatePost";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
       </Routes>
