f0c9b7f249a1d4d6f41fa187b82d9d6f130f26a7
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-29 20:16:15 +0900

    投稿ページデザイン

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./CreatePost.css";
 
 export const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを入力" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を入力"></textarea>
+        </div>
+        <button type="submit" className="postBtn">
+          投稿
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
