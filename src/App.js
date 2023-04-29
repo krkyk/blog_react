51dd541fdbd0813e83be74f2052e3e2ce2b38c45
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-29 15:59:54 +0900

    ログアウト機能作成

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Logout from "./components/Logout";
 import Login from "./components/Login";
 import CreatePost from "./components/CreatePost";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
 
   return (
     <Router>
-      <Navbar />
+      <Navbar isAuth={isAuth} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
+        <Route
+          path="/logout"
+          element={<Logout setIsAuth={setIsAuth} />}
+        ></Route>
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,19 +6,28 @@ import {
   faHouse,
   faPenNib,
   faRightToBracket,
+  faRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+const Navbar = ({isAuth}) => {
   return (
     <nav>
       <Link to="/">
         <FontAwesomeIcon icon={faHouse} />
         ホーム
       </Link>
-      <Link to="/login">
-        <FontAwesomeIcon icon={faRightToBracket} />
-        ログイン
-      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faRightToBracket} />
+          ログイン
+        </Link>
+      ) : (
+        <Link to="/logout">
+          <FontAwesomeIcon icon={faRightFromBracket} />
+          ログアウト
+        </Link>
+      )}
+
       <Link to="/createpost">
         <FontAwesomeIcon icon={faPenNib} />
         記事投稿
