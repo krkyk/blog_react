6b2b5c7e55095aea7920f5087b27c08637be924d
krkyk <100661771+krkyk@users.noreply.github.com>
2023-05-28 13:22:07 +0900

    homeコンポーネント作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+
+        <div className="postTextContainer">text</div>
+        <div className="nameAndDeleteButton">
+          <h3>name</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
+};
